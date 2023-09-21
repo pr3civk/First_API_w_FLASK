47ccc55875b9541ba6c186b0ec76c5758485f5c2
Piotr Preciuk <piotr.preciukbusiness@gmail.com>
2023-09-21 20:43:44 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_smorest import Api
+
+from resources.item import blueprint as Itblp
+from resources.store import blueprint as Stblp
+
+app = Flask(__name__)
+
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+api = Api(app)
+
+api.register_blueprint(Itblp)
+api.register_blueprint(Stblp)
